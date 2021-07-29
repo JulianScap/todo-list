dedf20411e8e3cfc0ae5765c424df3afc11533f9
Julian Adler <julian.a@passport.com>
2021-07-29 20:30:11 +1200

    Now using command line ui

---
+++ todo-list.console/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoTool
+{
+    internal static class Program
+    {
+        private static readonly List<string> todoList = new List<string>
+        {
+            "Go to school",
+            "Eat bread",
+            "Have a nap"
+        };
+
+        static void Main()
+        {
+            DisplayMenu();
+            ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+
+            while (keyPressed.Key != ConsoleKey.Q)
+            {
+                if (keyPressed.Key == ConsoleKey.A)
+                {
+                    DisplayAddMenu();
+                }
+                else if (keyPressed.Key == ConsoleKey.R)
+                {
+                    DisplayRemoveMenu();
+                }
+
+                DisplayMenu();
+                keyPressed = Console.ReadKey(true);
+            }
+        }
+
+        private static void DisplayRemoveMenu()
+        {
+            Console.Clear();
+            PrintTodoList();
+
+            Console.WriteLine("What item number do you want to remove?");
+            string numberAsString = Console.ReadLine();
+
+            if (int.TryParse(numberAsString, out int itemNumber)
+                && itemNumber >= 1
+                && itemNumber <= todoList.Count)
+            {
+                todoList.RemoveAt(itemNumber - 1);
+            }
+        }
+
+        private static void PrintTodoList()
+        {
+            var counter = 1;
+
+            foreach (string item in todoList)
+            {
+                Console.WriteLine($"{counter} - {item}");
+                counter += 1;
+            }
+        }
+
+        private static void DisplayAddMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter your todo:");
+            todoList.Add(Console.ReadLine());
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.Clear();
+            PrintTodoList();
+            Console.WriteLine("MENU - [A]dd - [R]emove - [Q]uit");
+        }
+    }
+}
